f782325ff81f0bd053751db28f9c518d5c3250d2
Will Dampier <judowill@gmail.com>
2017-01-25 15:08:45 -0500

    Basic commit of transformers

---
+++ crisprtree/preprocessing.py
@@ -0,0 +1,169 @@
+from sklearn.base import BaseEstimator
+import numpy as np
+
+
+class MatchingTransformer(BaseEstimator):
+    """ MatchingTransformer
+    This class is used to transform pairs of gRNA, Target rows into the simple matching encoding strategy. This strategy
+    represents the pair as a set of 20 + 1 binary elements. Each position is simply scored as "matching" or not. The 21st
+    position represents the NGG PAM site.
+        """
+
+    def fit(self, X, y):
+        """ fit
+        In this context nothing happens.
+        Parameters
+        ----------
+        X : np.array
+        y : np.array
+
+        Returns
+        -------
+        OneHotTransformer
+
+        """
+        return self
+
+    def transform(self, X):
+        """ transform
+        Transforms the array into the one-hot encoded format.
+
+        Parameters
+        ----------
+        X : np.array
+            This should be an Nx2 vector in which the first column is the gRNA
+            and the second column is the target sequence (+PAM).
+
+        Returns
+        -------
+        np.array
+
+        """
+
+        check_input(X)
+
+        encoded = []
+        for row in range(X.shape[0]):
+            encoded.append(match_encode_row(X[row, 0], X[row, 1]))
+
+        return np.array(encoded)
+
+
+class OneHotTransformer(BaseEstimator):
+    """ OneHotTransformer
+    This class is used to transform pairs of gRNA, Target rows into the One-Hot encoding strategy. This strategy
+    represents the pair as a set of 20*16 + 1 binary elements. Each match/mismatch is encoded individually A:A, A:C, A:G
+    A:T, C:A, etc.
+        """
+
+    def fit(self, X, y):
+        """ fit
+        In this context nothing happens.
+        Parameters
+        ----------
+        X : np.array
+        y : np.array
+
+        Returns
+        -------
+        OneHotTransformer
+
+        """
+        return self
+
+    def transform(self, X):
+        """ transform
+        Transforms the array into the one-hot encoded format.
+
+        Parameters
+        ----------
+        X : np.array
+            This should be an Nx2 vector in which the first column is the gRNA
+            and the second column is the target sequence (+PAM).
+
+        Returns
+        -------
+        np.array
+
+        """
+
+        check_input(X)
+
+        encoded = []
+        for row in range(X.shape[0]):
+            encoded.append(one_hot_encode_row(X[row, 0], X[row, 1]))
+
+        return np.array(encoded)
+
+
+def check_input(X):
+    """ Basic input parameter checking.
+    Parameters
+    ----------
+    X : np.array
+        This should be an Nx2 vector in which the first column is the gRNA
+        and the second column is the target sequence (+PAM).
+
+    Returns
+    -------
+    bool
+
+    """
+
+    assert X.shape[1] == 2
+
+    gRNA_lens = np.array([len(val) for val in X[:,0]])
+    hit_lens = np.array([len(val) for val in X[:,1]])
+
+    assert np.all(gRNA_lens == 20)
+    assert np.all(hit_lens == 23)
+
+    return True
+
+
+def match_encode_row(gRNA, target):
+    """ Does the actual match-based encoding.
+
+    Parameters
+    ----------
+    gRNA : str
+    target : str
+
+    Returns
+    -------
+    np.array
+
+    """
+
+    seq_order = 'ACGT'
+
+    features = [g == l for g, l in zip(gRNA, target)]
+    features.append(target[-2:] == 'GG')
+
+    return np.array(features)
+
+
+
+def one_hot_encode_row(gRNA, target):
+    """ Does the actual one-hot encoding using a set of nested for-loops.
+
+    Parameters
+    ----------
+    gRNA : str
+    target : str
+
+    Returns
+    -------
+    np.array
+
+    """
+
+    seq_order = 'ACGT'
+    features = []
+    for pos in range(20):
+        for g in seq_order:
+            for t in seq_order:
+                features.append((gRNA[pos] == g) and (target[pos] == t))
+    features.append(target[-2:]=='GG')
+
+    return np.array(features)
---
+++ test/test_preprocessing.py
@@ -0,0 +1,156 @@
+from crisprtree import preprocessing
+import pytest
+import numpy as np
+
+from crisprtree.preprocessing import one_hot_encode_row, check_input, match_encode_row
+
+
+class TestBasicInputs(object):
+
+    def test_simple_inputs(self):
+
+        gRNA = 'A'*20
+        hit = 'A'*20 + 'AGG'
+
+        inp = np.array([[gRNA, hit],
+                         [gRNA, hit]])
+
+        assert check_input(inp)
+
+    def test_missing_col(self):
+
+        gRNA = 'A'*20
+        hit = 'A'*20 + 'AGG'
+
+        inp = np.array([[gRNA],
+                         [gRNA]])
+
+        checks = [check_input,
+                  preprocessing.MatchingTransformer().transform,
+                  preprocessing.OneHotTransformer().transform
+                 ]
+
+        for check in checks:
+            with pytest.raises(AssertionError):
+                check(inp)
+
+    def test_missing_PAM(self):
+
+        gRNA = 'A'*20
+        hit = 'A'*20
+
+        inp = np.array([[gRNA, hit],
+                         [gRNA, hit]])
+
+        checks = [check_input,
+                  preprocessing.MatchingTransformer().transform,
+                  preprocessing.OneHotTransformer().transform
+                 ]
+
+        for check in checks:
+            with pytest.raises(AssertionError):
+                check(inp)
+
+
+class TestOneHotEncoding(object):
+
+    def test_encoding(self):
+
+        gRNA = 'A'*20
+        hit = 'A'*20 + 'AGG'
+
+        cor = np.zeros(20*16)
+        locs = np.arange(0, 20*16, 16)
+        cor[locs] = True
+
+        res = one_hot_encode_row(gRNA, hit)
+        assert res.shape == (20*16+1, )
+
+        np.testing.assert_array_equal(cor.astype(bool), res[:20*16])
+        assert res[-1] == True
+
+    def test_more_encoding(self):
+
+        gRNA = 'T' + 'A'*19
+        hit = 'A'*20 + 'AGG'
+
+        cor = np.zeros(20*16)
+        locs = np.arange(0, 20*16, 16)
+        cor[locs] = True
+        cor[0] = False
+        cor[12] = True
+
+        res = one_hot_encode_row(gRNA, hit)
+
+        assert res.shape == (20*16+1, )
+
+        np.testing.assert_array_equal(cor.astype(bool), res[:20*16])
+        assert res[-1] == True
+
+    def test_transforming(self):
+
+        gRNA = 'T' + 'A'*19
+        hit = 'A'*20 + 'AGG'
+
+        cor = np.zeros(20*16)
+        locs = np.arange(0, 20*16, 16)
+        cor[locs] = True
+        cor[0] = False
+        cor[12] = True
+
+        inp = np.array([[gRNA, hit],
+                        [gRNA, hit],
+                        [gRNA, hit]])
+        hot_encoder = preprocessing.OneHotTransformer()
+        res = hot_encoder.transform(inp)
+
+        assert res.shape == (3, 20*16+1)
+
+        for row in range(3):
+            np.testing.assert_array_equal(cor.astype(bool), res[row,:20*16])
+            assert res[row,-1] == True
+
+
+class TestMatchingEncoding(object):
+
+    def test_encoding(self):
+
+        gRNA = 'A'*20
+        hit = 'A'*20 + 'AGG'
+        cor = np.array([True]*21)
+
+        res = match_encode_row(gRNA, hit)
+        assert res.shape == (20+1, )
+
+        np.testing.assert_array_equal(cor.astype(bool), res)
+
+    def test_more_encoding(self):
+
+        gRNA = 'T' + 'A'*19
+        hit = 'A'*20 + 'AGG'
+
+        cor = np.array([False] + [True]*20)
+
+        res = match_encode_row(gRNA, hit)
+
+        assert res.shape == (20+1, )
+
+        np.testing.assert_array_equal(cor.astype(bool), res)
+
+    def test_transforming(self):
+
+        gRNA = 'T' + 'A'*19
+        hit = 'A'*20 + 'AGG'
+
+        cor = np.array([False] + [True]*20)
+
+        inp = np.array([[gRNA, hit],
+                        [gRNA, hit],
+                        [gRNA, hit]])
+        hot_encoder = preprocessing.MatchingTransformer()
+        res = hot_encoder.transform(inp)
+
+        assert res.shape == (3, 20+1)
+
+        for row in range(3):
+            np.testing.assert_array_equal(cor.astype(bool), res[row,:])
