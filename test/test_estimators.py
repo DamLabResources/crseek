a0f63a11940361483ec90dfb2fe8f717e003b20a
Will Dampier <judowill@gmail.com>
2017-04-15 21:29:43 -0400

    added some sanity checking for setting properties

--- test/test_estimators.py
+++ test/test_estimators.py
@@ -7,11 +7,25 @@ from itertools import cycle
 
 
 def make_match_array_from_seqs(grna, seqs):
+    """
+    Utility function for creating a MatchArray (Nx21 boolean) from a list
+    of sequences.
+
+    Parameters
+    ----------
+    grna : str
+        A 20 bp gRNA
+    seqs : iter
+        An iterable of 23bp target sequences
+
+    Returns
+    -------
+    np.array
+
+    """
 
     seq_array = np.array(zip(cycle([grna]), seqs))
     return preprocessing.MatchingTransformer().transform(seq_array)
-
-
 
 
 class TestMismatchEstimator(object):
@@ -41,4 +55,81 @@ class TestMismatchEstimator(object):
 
         np.testing.assert_array_equal(res, expected)
 
+    def test_set_seed_length(self):
 
+        grna = 'A'*20
+        hits = ['A'*20 + 'AGG',
+                'A'*19 + 'T' + 'AGG',
+                'A'*18 + 'TA' + 'AGG',
+                'A'*17 + 'TAA' + 'AGG',
+                'A'*16 + 'TAAA' + 'AGG',]
+
+        match_array = make_match_array_from_seqs(grna, hits)
+
+        mod = estimators.MismatchEstimator(seed_len = 2,
+                                           miss_seed = 0,
+                                           miss_non_seed = 2,
+                                           require_pam = True)
+        res = mod.predict(match_array)
+        expected = [True, False, False, True, True]
+        np.testing.assert_array_equal(res, expected)
+
+        mod = estimators.MismatchEstimator(seed_len = 3,
+                                           miss_seed = 0,
+                                           miss_non_seed = 2,
+                                           require_pam = True)
+        res = mod.predict(match_array)
+        expected = [True, False, False, False, True]
+        np.testing.assert_array_equal(res, expected)
+
+    def test_set_seed_mismatch(self):
+
+        grna = 'A'*20
+        hits = ['A'*20 + 'AGG',
+                'A'*19 + 'T' + 'AGG',
+                'A'*18 + 'TT' + 'AGG',
+                'A'*17 + 'TTT' + 'AGG',]
+
+        match_array = make_match_array_from_seqs(grna, hits)
+
+        mod = estimators.MismatchEstimator(seed_len = 4,
+                                           miss_seed = 1,
+                                           miss_non_seed = 2,
+                                           require_pam = True)
+        res = mod.predict(match_array)
+        expected = [True, True, False, False]
+        np.testing.assert_array_equal(res, expected)
+
+        mod = estimators.MismatchEstimator(seed_len = 4,
+                                           miss_seed = 2,
+                                           miss_non_seed = 2,
+                                           require_pam = True)
+        res = mod.predict(match_array)
+        expected = [True, True, True, False]
+        np.testing.assert_array_equal(res, expected)
+
+    def test_set_non_seed_mismatch(self):
+
+        grna = 'A'*20
+        hits = ['A'*20 + 'AGG',
+                'T' + 'A'*19 + 'AGG',
+                'TT' + 'A'*18 + 'AGG',
+                'TTT' + 'A'*17 + 'AGG']
+
+        match_array = make_match_array_from_seqs(grna, hits)
+
+        mod = estimators.MismatchEstimator(seed_len = 4,
+                                           miss_seed = 0,
+                                           miss_non_seed = 2,
+                                           require_pam = True)
+        res = mod.predict(match_array)
+        expected = [True, True, True, False]
+        np.testing.assert_array_equal(res, expected)
+
+        mod = estimators.MismatchEstimator(seed_len = 4,
+                                           miss_seed = 0,
+                                           miss_non_seed = 1,
+                                           require_pam = True)
+        res = mod.predict(match_array)
+        expected = [True, True, False, False]
+        np.testing.assert_array_equal(res, expected)
