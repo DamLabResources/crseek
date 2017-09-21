ec83b7a78bf4820c275f1deabd976baf05d75dd9
Will Dampier <judowill@gmail.com>
2017-09-21 09:52:01 -0400

    now properly deals with indexes in the evaluators functions

--- crisprtree/evaluators.py
+++ crisprtree/evaluators.py
@@ -4,7 +4,7 @@ from sklearn.base import BaseEstimator
 from Bio.Seq import reverse_complement
 
 
-def check_grna_across_seqs(grna, seqs, estimator, aggfunc='max', index=None):
+def check_grna_across_seqs(grna, seqs, estimator, index=None):
     """ Simple utility function to check all sequences against a single gRNA
 
     Parameters
@@ -15,8 +15,6 @@ def check_grna_across_seqs(grna, seqs, estimator, aggfunc='max', index=None):
         Iterable of sequences to check
     estimator : BaseEstimator
         The estimator to use for evaluation. The estimator should already be *fit*
-    aggfunc : str or func
-        How to aggregate the results from the same sequence. Anything understood by pandas.agg
     index : pd.Index
         An index to attach to the result
 
@@ -28,51 +26,62 @@ def check_grna_across_seqs(grna, seqs, estimator, aggfunc='max', index=None):
     """
 
     checks = []
+    seq_info = []
     orig_place = []
     orig_position = []
     orig_strand = []
 
     if type(seqs) == type(pd.Series()):
         it = zip(seqs.index, seqs.values)
+        index_name = seqs.index.name
     else:
         it = enumerate(seqs)
+        index_name = 'SeqNum'
 
     for seq_key, seq in it:
         if len(seq) < 23:
             # deal with short sequences gracefully
             checks.append((grna, 'X'*23))
-            orig_place.append(seq_key)
-            orig_position.append(-1)
-            orig_strand.append('')
+            seq_info.append({'Index': seq_key,
+                             'Position': -1,
+                             'Strand': ''})
             continue
 
         rseq = reverse_complement(seq)
         for n in range(len(seq)-23):
             if all(l.upper() in {'A', 'C', 'G', 'T'} for l in str(seq[n:n+23])):
                 checks.append((grna, seq[n:n+23]))
-                orig_place.append(seq_key)
-                orig_position.append(n)
-                orig_strand.append('+')
+                seq_info.append({'Index': seq_key,
+                                 'Position': n,
+                                 'Strand': '+'})
 
             if all(l.upper() in {'A', 'C', 'G', 'T'} for l in str(rseq[n:n+23])):
                 checks.append((grna, rseq[n:n+23]))
-                orig_place.append(seq_key)
-                orig_position.append(n)
-                orig_strand.append('-')
-
+                seq_info.append({'Index': seq_key,
+                                 'Position': n,
+                                 'Strand': '-'})
 
     res = estimator.predict_proba(np.array(checks))
 
     targets = [targ for grna, targ in checks]
 
-    df = pd.DataFrame({'SeqNum': orig_place, 'Value': res, 'Target': targets,
-                    'Position': orig_position, 'Strand': orig_strand})
+    df = pd.DataFrame(seq_info)
+    df['Target'] = targets
+    df['Value'] = res
 
     def fix_agg(rows):
         idx = rows['Value'].idxmax()
         return rows.ix[idx]
 
 
-    out = df.groupby('SeqNum')[['Value', 'Target', 'Position', 'Strand']].agg(fix_agg)
+    out = df.groupby('Index')[['Value', 'Target', 'Position', 'Strand']].agg(fix_agg)
+    out.index.name = index_name
+
+    if index is not None:
+        out.index = index
+
     return out
 
+
+def positional_aggregation():
+    pass
